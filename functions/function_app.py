5af55526af6d59b3e7dc2711253f75e42f018ed5
Florian Thiemer <fthiemer@gmail.com>
2024-05-25 23:46:40 +0200

    add container start to function

--- functions/function_app.py
+++ functions/function_app.py
@@ -1,25 +1,37 @@
-import azure.functions as func
-import logging
+import json
+import requests
+from azure.functions import HttpRequest, HttpResponse
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.containerinstance import ContainerInstanceManagementClient
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-@app.route(route="UploadOntology")
-def UploadOntology(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+def main(req: HttpRequest) -> HttpResponse:
+    action = req.params.get('action')
+    credential = DefaultAzureCredential()
+    client = ContainerInstanceManagementClient(credential, 'b908a739-b5f8-4fe2-8572-90c18b31355c')
+    resource_group = 'my-ontologies_group'
+    container_group_name = 'fuseki'
+    
+    if action == 'upload':
+        # Start the container
+        client.container_groups.start(resource_group, container_group_name)
+        
+        ontology = req.files['ontology'].read()
+        response = requests.post('http://flocontainer.ctfvdbc0b2faawej.germanywestcentral.azurecontainer.io:3030/dataset/data', files={'file': ontology})
+        
+        # Stop the container
+        client.container_groups.stop(resource_group, container_group_name)
+        
+        return HttpResponse(response.content, status_code=response.status_code)
+    elif action == 'query':
+        # Start the container
+        client.container_groups.start(resource_group, container_group_name)
+        
+        query = req.params.get('query')
+        response = requests.post('http://flocontainer.ctfvdbc0b2faawej.germanywestcentral.azurecontainer.io:3030/dataset/sparql', data={'query': query})
+        
+        # Stop the container
+        client.container_groups.stop(resource_group, container_group_name)
+        
+        return HttpResponse(response.content, status_code=response.status_code)
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return HttpResponse("Invalid action.", status_code=400)
